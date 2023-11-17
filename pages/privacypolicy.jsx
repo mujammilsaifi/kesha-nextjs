f29afcaedb93819879870211e6ee02dfaef69060
Mujammil <116416462+mujammilsaifi@users.noreply.github.com>
2023-11-17 11:24:47 +0530

    error fix

--- pages/privacypolicy.jsx
+++ pages/privacypolicy.jsx
@@ -1,109 +1,154 @@
-import React from 'react'
+import React from 'react';
 
 const Privacypolicy = () => {
-    return (
-        <section className='my-[10vh] w-full bg-gray-100 p-4 min-h-[60vh] flex flex-col justify-center items-center '>
-            <div className="w-full p-8 bg-white rounded-md">
-                <h1 className="text-3xl font-bold font-oswald">Privacy Policy</h1>
-                <p className="text-gray-600 mb-6">Last Updated: October 2023</p>
-                <p className="text-gray-800 mb-4">
-                    At Kesha Jewellery, we are dedicated to protecting your privacy and safeguarding your
-                    personal information.
-                </p>
-                <p className="text-gray-800 mb-4">
-                    Our Privacy Policy outlines how we collect, use, and disclose information when you use our
-                    services.
-                </p>
-                <p className="text-gray-800 mb-4">
-                    We encourage you to read our Privacy Policy carefully so that you understand how your
-                    personal information is handled.
-                </p>
+  return (
+    <section className="my-[10vh] w-full bg-gray-100 p-4 min-h-[60vh] flex flex-col justify-center items-center">
+      <div className="w-full p-8 bg-white rounded-md">
+        <h1 className="text-3xl font-bold font-oswald">Privacy Policy</h1>
+        <p className="text-gray-600 mb-6">Last Updated: October 2023</p>
+        <p className="text-gray-800 mb-4">
+          At Kesha Jewellery, we are dedicated to protecting your privacy and safeguarding your
+          personal information.
+        </p>
 
-                <p className="text-gray-800 mb-4">
-                    Welcome to Kesha Jewellery. Our website is the property of Kesha Jewellery Pvt. Ltd. By accessing and using the Site at www.KeshaJewellery.com, you (referred to as "Users," "you," or "your") agree to the practices outlined in this Privacy Policy. Please read this policy carefully before submitting any personal data to Kesha Jewellery through the Site. If you have any questions regarding this Privacy Policy, please contact us at keshajewels@gmail.com
-                </p>
+        <p className="text-gray-800 mb-4">
+          Our Privacy Policy outlines how we collect, use, and disclose information when you use our
+          services.
+        </p>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Information Collection:</h1>
+        <p className="text-gray-800 mb-4">
+          We encourage you to read our Privacy Policy carefully so that you understand how your
+          personal information is handled.
+        </p>
 
-                <p className="text-gray-800 mb-4">
-                    When you visit, use, or interact with the Site, we collect certain information about your visit, use, or interactions. This includes monitoring the number of visitors, peak hours of visits, visited pages, referring domains, browsers used, broad geographical information, and site-navigation patterns. The following information is automatically logged in our systems:
-                </p>
-                <p className="text-gray-800 mb-4">
-                    *Log Data:* Your browser automatically sends information ("log data") whenever you visit the Site. Log data includes your IP address (to determine your country of connection), browser type and settings, request date and time, and interactions with the Site.
-                </p>
-                <p className="text-gray-800 mb-4">
-                    *Information from Cookies and Technologies:* Temporary cookies are used for technical administration, research, and development purposes. These cookies do not store personally identifiable information. Additionally, authorized third parties may place or recognize unique cookies on your browser when serving advertisements or optimizing services.
-                </p>
+        <p className="text-gray-800 mb-4">
+          Welcome to Kesha Jewellery. Our website is the property of Kesha Jewellery Pvt. Ltd. By
+          accessing and using the Site at www.KeshaJewellery.com, you (referred to as "Users," "you," or
+          "your") agree to the practices outlined in this Privacy Policy. Please read this policy carefully
+          before submitting any personal data to Kesha Jewellery through the Site. If you have any
+          questions regarding this Privacy Policy, please contact us at keshajewels@gmail.com
+        </p>
 
-                <p className="text-gray-800 mb-4">
-                    We do not have control over the privacy practices or policies of third-party websites or the accuracy, integrity, or quality of the information available on those websites. It is recommended to review the privacy policies of any third-party websites.
-                </p>
-                <p className="text-gray-800 mb-4">
-                    *Device Information:* We collect device-specific information, including the device name, operating system, and browser you are using, depending on the type of device and its settings.
-                </p>
-                <p className="text-gray-800 mb-4">
-                    *Usage Information:* We collect information about how you use our Site, such as the types of content you view or engage with, the features you use, the actions you take, and the time, frequency, and duration of your activities.
-                </p>
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Information Collection:</h1>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Information Use:</h1>
-                <p className="text-gray-800 mb-4">
-                    We use your Personal Data for the following purposes:
-                </p>
-                <p className="text-gray-800 mb-4">
-                    - Processing Orders<br />
-                    - Responding to Inquiries<br />
-                    - Customizing User Experience<br />
-                    - Sending Administrative Information<br />
-                    - Analyzing Site Usage<br />
-                    - Conducting Analytics<br />
-                    - Fraud Prevention and Security<br />
-                    - Compliance and Legal Obligations<br />
-                    - Marketing<br />
-                    - Contests, Surveys, and Promotions<br />
-                    - Targeted Advertisements<br />
-                </p>
+        <p className="text-gray-800 mb-4">
+          When you visit, use, or interact with the Site, we collect certain information about your visit, use,
+          or interactions. This includes monitoring the number of visitors, peak hours of visits, visited
+          pages, referring domains, browsers used, broad geographical information, and site-navigation
+          patterns. The following information is automatically logged in our systems:
+        </p>
 
-                <h1 className="text-3xl font-bold font-oswald mb-6">Sharing and Disclosure:</h1>
-                <p className="text-gray-800 mb-4">
-                    In certain circumstances, we may share your Personal Data with third parties, such as service providers, business partners, or affiliates. We may also share your information in the event of a merger, acquisition, or sale of our company, as required by law, or to protect the rights, property, or safety of Kesha Jewellery, its users, or others.
-                </p>
+        <p className="text-gray-800 mb-4">
+          *Log Data:* Your browser automatically sends information ("log data") whenever you visit the Site.
+          Log data includes your IP address (to determine your country of connection), browser type and
+          settings, request date and time, and interactions with the Site.
+        </p>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Data Security:</h1>
-                <p className="text-gray-800 mb-4">
-                    We implement reasonable security measures to protect your Personal Data. However, please note that no system is completely secure. We cannot guarantee the security of your information transmitted to our Site.
-                </p>
+        <p className="text-gray-800 mb-4">
+          *Information from Cookies and Technologies:* Temporary cookies are used for technical
+          administration, research, and development purposes. These cookies do not store personally
+          identifiable information. Additionally, authorized third parties may place or recognize unique
+          cookies on your browser when serving advertisements or optimizing services.
+        </p>
 
+        <p className="text-gray-800 mb-4">
+          We do not have control over the privacy practices or policies of third-party websites or the
+          accuracy, integrity, or quality of the information available on those websites. It is recommended to
+          review the privacy policies of any third-party websites.
+        </p>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Children's Privacy:</h1>
-                <p className="text-gray-800 mb-4">
-                    The Site is not intended for use by individuals under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that we have collected personal information from a child under the age of 13 without parental consent, we will take steps to delete that information.
-                </p>
+        <p className="text-gray-800 mb-4">
+          *Device Information:* We collect device-specific information, including the device name,
+          operating system, and browser you are using, depending on the type of device and its settings.
+        </p>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Third-Party Links:</h1>
-                <p className="text-gray-800 mb-4">
-                    Our Site may contain links to third-party websites. These websites have their own privacy policies, and we do not accept any responsibility or liability for their policies or the accuracy, integrity, or quality of the information available on those websites.
-                </p>
+        <p className="text-gray-800 mb-4">
+          *Usage Information:* We collect information about how you use our Site, such as the types of
+          content you view or engage with, the features you use, the actions you take, and the time, frequency,
+          and duration of your activities.
+        </p>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Changes to the Privacy Policy:</h1>
-                <p className="text-gray-800 mb-4">
-                    We reserve the right to update or modify this Privacy Policy at any time. Changes will be effective immediately upon posting the updated Privacy Policy on the Site.
-                </p>
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Information Use:</h1>
 
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Processing Location:</h1>
-                <p className="text-gray-800 mb-4">
-                    We ensure the appropriate security of your Personal Data based on its processing location.
-                </p>
-                <h1 className="text-3xl font-bold mb-6 font-oswald">Contact Information:</h1>
-                <p className="text-gray-800 mb-4">
-                    If you have any questions or concerns about this Privacy Policy, please contact us at keshajewels@gmail.com
-                </p>
-                <p className="text-gray-800 mb-4">
-                    Thank you for reading our Privacy Policy. By using our Site, you consent to the practices outlined in this Privacy Policy.
-                </p>
-                {/* Add more content for the Privacy Policy page here */}
-            </div>
-        </section >
-    );
+        <p className="text-gray-800 mb-4">
+          We use your Personal Data for the following purposes:
+        </p>
+
+        <p className="text-gray-800 mb-4">
+          - Processing Orders<br />
+          - Responding to Inquiries<br />
+          - Customizing User Experience<br />
+          - Sending Administrative Information<br />
+          - Analyzing Site Usage<br />
+          - Conducting Analytics<br />
+          - Fraud Prevention and Security<br />
+          - Compliance and Legal Obligations<br />
+          - Marketing<br />
+          - Contests, Surveys, and Promotions<br />
+          - Targeted Advertisements<br />
+        </p>
+
+        <h1 className="text-3xl font-bold font-oswald mb-6">Sharing and Disclosure:</h1>
+
+        <p className="text-gray-800 mb-4">
+          In certain circumstances, we may share your Personal Data with third parties, such as service
+          providers, business partners, or affiliates. We may also share your information in the event of a
+          merger, acquisition, or sale of our company, as required by law, or to protect the rights, property, or
+          safety of Kesha Jewellery, its users, or others.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Data Security:</h1>
+
+        <p className="text-gray-800 mb-4">
+          We implement reasonable security measures to protect your Personal Data. However, please note
+          that no system is completely secure. We cannot guarantee the security of your information
+          transmitted to our Site.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Children's Privacy:</h1>
+
+        <p className="text-gray-800 mb-4">
+          The Site is not intended for use by individuals under the age of 13. We do not knowingly collect
+          personal information from children under 13. If we become aware that we have collected personal
+          information from a child under the age of 13 without parental consent, we will take steps to delete
+          that information.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Third-Party Links:</h1>
+
+        <p className="text-gray-800 mb-4">
+          Our Site may contain links to third-party websites. These websites have their own privacy policies,
+          and we do not accept any responsibility or liability for their policies or the accuracy, integrity, or
+          quality of the information available on those websites.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Changes to the Privacy Policy:</h1>
+
+        <p className="text-gray-800 mb-4">
+          We reserve the right to update or modify this Privacy Policy at any time. Changes will be effective
+          immediately upon posting the updated Privacy Policy on the Site.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Processing Location:</h1>
+
+        <p className="text-gray-800 mb-4">
+          We ensure the appropriate security of your Personal Data based on its processing location.
+        </p>
+
+        <h1 className="text-3xl font-bold mb-6 font-oswald">Contact Information:</h1>
+
+        <p className="text-gray-800 mb-4">
+          If you have any questions or concerns about this Privacy Policy, please contact us at
+          keshajewels@gmail.com
+        </p>
+
+        <p className="text-gray-800 mb-4">
+          Thank you for reading our Privacy Policy. By using our Site, you consent to the practices outlined
+          in this Privacy Policy.
+        </p>
+      </div>
+    </section>
+  );
 };
 
-export default Privacypolicy;
+export default Privacypolicy;
